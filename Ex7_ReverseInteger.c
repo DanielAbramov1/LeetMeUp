0b0ee960065137a478e00fde08543e39fa47f97a
DnA3005 <danielabramov32@gmail.com>
2024-03-20 19:27:09 +0200

    Exercise finished

--- Ex7_ReverseInteger.c
+++ Ex7_ReverseInteger.c
@@ -23,12 +23,14 @@ Output: 21
 
 Constraints:
 
--231 <= x <= 231 - 1
+-2^31 <= x <= 2^31 
 
 ----------------------------------------------------------------------------------------------------------------------------------
 */
 
+#include <stdlib.h>
 #include <stdio.h>
+#include <math.h>
 
 #define true    1
 #define false   0
@@ -37,14 +39,48 @@ int reverse(int x)
 {
     // first edge case
     if(x == 0)
+    {
         return 0;
+    }
     
-    // 
+    // 2nd edge case if number is negative
+    int negativeFlag = false;
+    if(x < 0)
+    {
+        negativeFlag = true;
+    }
 
+    // save the absolute value for processing
+    x = abs(x);
+    int xReversed = 0;
+
+    while(x > 0)
+    {
+        xReversed = (xReversed*10) + (x%10);
+        x = x/10;
+    }
+
+    // turn to negtive if flag was raised
+    if(negativeFlag)
+    {
+        xReversed = -1*xReversed;
+    }
+
+    return xReversed;
 }
 
+/* Code for manual check
 void main()
 {
+    int inputNum;
+    while(1)
+    {
+        printf("please enter a number -2^31 <= num <= 2^31:\n");
+        scanf("%d", &inputNum);
+        printf("you entered the num :%d\n",inputNum);
 
+        printf("the reversed integer is :%d\n", reverse(inputNum));
+    }
     return;
 }
+*/
