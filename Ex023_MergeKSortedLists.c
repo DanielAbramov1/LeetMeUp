901b9f1df625cc827e0151a68c9c44afce7a322c
DnA3005 <danielabramov32@gmail.com>
2024-03-27 16:22:55 +0200

    work in progress

--- Ex023_MergeKSortedLists.c
+++ Ex023_MergeKSortedLists.c
@@ -45,9 +45,131 @@ The sum of lists[i].length will not exceed 104.
  *     int val;
  *     struct ListNode *next;
  * };
- */
+ * 
+*/
 
+#include "common.h"
+#include <stdbool.h>
+
+typedef struct ListNode 
+{
+  int val;
+  struct ListNode *next;
+}ListNode_t;
+
+// create new node
+ListNode_t * createNode(void)
+{
+  ListNode_t * p = (ListNode_t*)malloc(sizeof(ListNode_t));
+  if(p == NULL)
+  { 
+    printf("Couldn't allocate memory <>\n");
+    return NULL;
+  }
+
+  p->val = 0;
+  p->next = NULL;
+
+  return p;
+}
+
+// returns true if all lists in the array are empty
+bool isEmptyListArray(struct ListNode** lists, int listsSize) 
+{
+  int k = 0;
+  int countEmptyLists = 0;
+
+  while(k < listsSize)
+  {
+    if(*(lists + k) == NULL)
+      countEmptyLists++;
+    else
+      break;
+
+    k++;
+  }
+
+  if(countEmptyLists == listsSize)
+    return true;
+  else
+    return false;
+}
+
+// returns the index of the first non null list
+int firstValidListIndex (struct ListNode** lists, int listsSize)
+{
+  int index = -1;
+
+  for (int k = 0; k < listsSize; k++)
+  {
+    if(*(lists + k) != NULL)
+    {
+      index = k;
+      break;
+    }
+  }
+
+  return index;
+}
+
+// returns the index of the smallest value in the  first nodes in the array of lists
+int smallestValIndex(struct ListNode** lists, int listsSize)
+{
+  // store the first element val of the first list found
+  int smallestValIndex = firstValidListIndex(lists, listsSize);
+  int currentVal = (*(lists + smallestValIndex))->val;
+  int minVal = currentVal;
+
+  for (int k = smallestValIndex+1; k < listsSize; k++)
+  {
+    if(*(lists + k) != NULL)
+    { 
+      currentVal = (*(lists + k))->val;
+      
+      if(currentVal < minVal)
+      {
+        minVal = currentVal;
+        smallestValIndex = k;
+      }
+    }
+  }
+
+  return smallestValIndex;
+}
+
+//TODO continue here - here we need to trim and rearrange the pointers
 struct ListNode* mergeKLists(struct ListNode** lists, int listsSize) 
 {
-    
+  if(isEmptyListArray(lists, listsSize))
+  {
+    printf("All the lists in the list array are empty\n");
+    return NULL;
+  }
+
+  return NULL;
+}
+
+
+void main()
+{
+  struct ListNode * list2 = createNode();
+  struct ListNode * list4 = createNode();
+  struct ListNode * list5 = createNode();
+
+  list2->val = 3;
+  list4->val = 1;
+  list5->val = 5;
+
+  struct ListNode * lists[] = {NULL, list2, NULL, list4, list5};
+
+  printf("the smallest is in list number:%d\n",smallestValIndex(lists, sizeof(lists)/sizeof(lists[0])) + 1);
+
+  // struct ListNode * merged = createNode();
+  // merged = mergeKLists(lists, sizeof(lists)/sizeof(lists[0]));
+
+  free(list2);
+  free(list4);
+  free(list5);
+  // free(merged);
+  return;
 }
