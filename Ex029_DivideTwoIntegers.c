84b0c8d8ad27b44f0f37e9cd794ea2474ce1751e
DnA3005 <danielabramov32@gmail.com>
2024-04-04 15:25:47 +0300

    Exercise finished - bit manipulation

--- Ex029_DivideTwoIntegers.c
+++ Ex029_DivideTwoIntegers.c
@@ -30,13 +30,87 @@ divisor != 0
 ----------------------------------------------------------------------------------------------------------------------------------
 */
 
+#include "common.h"
+#include <stdint.h>
+
+bool isSigned(int num)
+{
+    if(num >= 0)
+        return false;
+    else
+        return true;
+}
+
+
+int64_t myAbsolute(int num)
+{
+    int64_t result;
+
+    if(num < 0)
+    {
+        int64_t temp = ~num;
+        result = temp +1 ;
+    }
+    else
+        result = num;
+
+    return result;
+}
+
 int divide(int dividend, int divisor) 
 {
+    // xor operator will return the sing between 2 numbers
+    bool signReturn = isSigned(dividend) ^ isSigned(divisor);
+
+    int count = 0;
+
+    // working with absolutes
+    int64_t absDividend = myAbsolute(dividend);
+    int64_t absDivider = myAbsolute(divisor);
+
+    // edge case for out of int bounds
+    if(dividend == INT32_MIN && absDivider == 1) 
+    {
+        if(isSigned(divisor))
+            return INT32_MAX;
+        else // advisor == 1
+            return INT32_MIN;
+    }
+
+    // reducing the dividend until he is less then 0
+    while(absDividend >= 0)
+    {
+        if(absDividend - absDivider >= 0)
+        {
+            count++;
+        }
+        absDividend = absDividend - absDivider;
+    }
     
+    // changing the sign if one of the nums is signed
+    if(signReturn)
+        count = (~count) + 1;
+
+    return count;
+
 }
 
 void main()
 {
-    
+    int a = -2147483648;
+    int b = -1;
+    printf("Dividing test without multiply and devision operators on %d / %d = %d\n", a, b, divide(a,b));
+
+    a = 2147483647;
+    b = 1;
+    printf("Dividing test without multiply and devision operators on %d / %d = %d\n", a, b, divide(a,b));
+
+    a = -2147483648;
+    b = 1;
+    printf("Dividing test without multiply and devision operators on %d / %d = %d\n", a, b, divide(a,b));
+
+    a = 2147483647;
+    b = -1;
+    printf("Dividing test without multiply and devision operators on %d / %d = %d\n", a, b, divide(a,b));
     return;
 }
